4e165da000498d68203a550027ea9e7412e6713b
Andrey-phystech <91078587+Andrey-phystech@users.noreply.github.com>
2021-09-26 15:02:40 +0300

    Delete ex7.py

--- ex7.py
+++
@@ -1,8 +0,0 @@
-import turtle
-import math
-
-turtle.shape('turtle')
-for phi in range(1000):
-    x = 1 * (phi / 10) * math.cos(phi / 10)
-    y = 1 * (phi / 10) * math.sin(phi / 10)
-    turtle.goto(x, y)
